b894c8249417c89cb55c957687fa27724b37e00d
shannu <yourshannu1@gmail.com>
2020-12-29 20:41:55 +0530

    done

--- MongoDB_SpringBoot/src/main/java/com/sankar/tech/pro/MongoDbSpringBootApplication.java
+++ MongoDB_SpringBoot/src/main/java/com/sankar/tech/pro/MongoDbSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MongoDbSpringBootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("=====MongoDbSpringBootApplication=====");
+		System.out.println("=======MongoDbSpringBootApplication=======");
 		SpringApplication.run(MongoDbSpringBootApplication.class, args);
 	}
 
